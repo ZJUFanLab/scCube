5ebd966bf44e3df306fb0da5fd9a36cfe16dfb62
qianjingyang <qianjingyang@zju.edu.cn>
2023-05-12 14:48:39 +0800

    scCube v1.0.0

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="scCube",
+    version="1.0.0",
+    author='Jingyang Qian',
+    author_email='qianjingyang@zju.edu.cn',
+    url='https://github.com/ZJUFanLab/scCube',
+    packages=find_packages(),
+    install_requires=[],
+    zip_safe=False,
+    python_requires='>=3.8',
+    license='GNU General Public License v3.0',
+)
+
+
